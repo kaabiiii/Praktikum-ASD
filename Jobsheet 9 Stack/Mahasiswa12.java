bd2b760dd6ba432427992937f3c5b998d1baf611
kaabiiii <kaabimrfuad1@gmail.com>
2025-05-22 22:30:23 +0700

    Add files via upload

---
+++ Jobsheet 9 Stack/Mahasiswa12.java
@@ -0,0 +1,16 @@
+public class Mahasiswa12 {
+
+    String nama, nim, kelas;
+    int nilai;
+
+    Mahasiswa12(String nama, String nim, String kelas){
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+    void tugasDinilai (int nilai){
+        this.nilai = nilai;
+    }
+    
+}
